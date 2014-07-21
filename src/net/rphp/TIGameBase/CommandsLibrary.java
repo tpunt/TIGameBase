e85c3f2510bdd9ffefb52f7f070756e4ed3c2a41
tpunt <tpunt@hotmail.co.uk>
2014-07-21 22:36:47 +0100

    Fixed compile errors

--- src/net/rphp/TIGameBase/CommandsLibrary.java
+++ src/net/rphp/TIGameBase/CommandsLibrary.java
@@ -1,29 +1,41 @@
 package net.rphp.TIGameBase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CommandsLibrary
 {
 	private ArrayList<ArrayList<String>> commandWords;
 	private ArrayList<String> preGameCommandWords;
 	private static CommandsLibrary instance = null;
+	private HashMap<String, String> commandWordsManual;
 
 	private CommandsLibrary()
 	{
 		commandWords = new ArrayList<ArrayList<String>>();
 		preGameCommandWords = new ArrayList<String>();
+		commandWordsManual = new HashMap<String, String>();
+
+		commandWordsManual.put("new", "Load a new game. Syntax: 'new game'.");
+		commandWordsManual.put("load", "Load a saved game. Syntax: 'load game GAME_NAME'.");
+		commandWordsManual.put("quit", "Quit the in-game or pregame mode. Syntax: 'quit'.");
+		commandWordsManual.put("help", "Show help. Syntax: 'help'.");
+		commandWordsManual.put("manual", "Either show all valid commands or view information about a specific command. Syntax: 'manual' or 'manual COMMAND_NAME'.");
 
 		preGameCommandWords.add("new");
 		preGameCommandWords.add("load");
 		preGameCommandWords.add("quit");
 		preGameCommandWords.add("help");
+		preGameCommandWords.add("manual");
 
 		commandWords.add(new ArrayList<String>());
 		commandWords.get(0).add("quit");
 		commandWords.get(0).add("help");
+		commandWords.get(0).add("manual");
 
 		commandWords.add(new ArrayList<String>());
 		commandWords.get(1).add("new");
+		commandWords.get(1).add("manual");
 
 		commandWords.add(new ArrayList<String>());
 		commandWords.get(2).add("load");
@@ -61,5 +73,10 @@ public class CommandsLibrary
 			return true;
 
 		return false;
+	}
+
+	public ArrayList<String> getPreGameCommands()
+	{
+		return preGameCommandWords;
 	}
 }
